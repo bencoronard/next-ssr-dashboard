06a4a19ef0d07844653edc79add53fab742dbeda
Ben <128577990+bencoronard@users.noreply.github.com>
2025-02-03 20:48:02 +0700

    Add centralized properties

--- lb-dashboard-nextjs/properties.mjs
+++ lb-dashboard-nextjs/properties.mjs
@@ -1,16 +1,17 @@
 const properties = {
   server: {
-    database: {
-      url: process.env.DATABASE_URL || "url",
-      username: process.env.DB_USERNAME || "next",
-      password: process.env.DB_PASSWORD || "1234",
-    },
-    api: {
-      secretKey: process.env.SECRET_API_KEY || "loxbit888",
+    port: Number.parseInt(process.env.APP_SERVER_PORT || 3000, 10),
+    serviceId: process.env.PORTAL_SERVICE_FRONTEND_ID || "BFF",
+    secretKey: process.env.PORTAL_SERVICE_FRONTEND_SECRET_KEY || "Mammoth1234",
+    environment: process.env.PORTAL_SERVICE_ENVIRONMENT || "dev",
+  },
+  http: {
+    client: {
+      timeout: Number.parseInt(process.env.APP_HTTP_CLIENT_TIMEOUT || "10", 10),
     },
   },
-  client: {
-    publicApiUrl: process.env.PUBLIC_API_URL || "curl",
+  portal: {
+    baseUrl: process.env.PORTAL_BASE_URL || "http://localhost:80",
   },
 };
 
