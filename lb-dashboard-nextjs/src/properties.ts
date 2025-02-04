2aec23b4e6f4858c2703d3c02d5b8473b1825312
Ben <128577990+bencoronard@users.noreply.github.com>
2025-02-04 14:02:54 +0700

    Create properties.ts

---
+++ lb-dashboard-nextjs/src/properties.ts
@@ -0,0 +1,21 @@
+const properties = {
+  server: {
+    port: Number.parseInt(process.env.APP_SERVER_PORT || "3000", 10),
+    serviceId: process.env.PORTAL_SERVICE_FRONTEND_ID || "BFF",
+    secretKey: process.env.PORTAL_SERVICE_FRONTEND_SECRET_KEY || "Mammoth1234",
+    environment: process.env.PORTAL_SERVICE_ENVIRONMENT || "dev",
+  },
+  http: {
+    client: {
+      timeout: Number.parseInt(
+        process.env.APP_HTTP_CLIENT_TIMEOUT || "10000",
+        10
+      ),
+    },
+  },
+  portal: {
+    baseUrl: process.env.PORTAL_BASE_URL || "http://localhost:3000",
+  },
+};
+
+export default properties;
