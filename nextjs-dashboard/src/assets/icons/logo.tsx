e0da63ed06bd441c66b934fb17e3389142b61db0
Ben <128577990+bencoronard@users.noreply.github.com>
2025-02-07 11:32:32 +0700

    Add assets icon

---
+++ nextjs-dashboard/src/assets/icons/logo.tsx
@@ -0,0 +1,25 @@
+import { Box } from "@mui/material";
+import Image from "next/image";
+import React from "react";
+
+interface ResponsiveLogoProps {
+  variant?: "dark" | "light" | "neutral";
+}
+
+export default function ResponsiveLogo(props: ResponsiveLogoProps) {
+  console.log("ResponsiveLogo() was rendered here");
+
+  const { variant = "neutral" } = props;
+
+  return (
+    <Box sx={{ width: "100%", height: "100%", position: "relative" }}>
+      <Image src={source[variant]} alt="Logo" fill={true} priority />
+    </Box>
+  );
+}
+
+const source = {
+  neutral: "/assets/images/logo/logo-neutral.svg",
+  dark: "/assets/images/logo/logo-dark.svg",
+  light: "/assets/images/logo/logo-light.svg",
+};
