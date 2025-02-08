e8ae1c9eaccb6789a8a8001019d55d7ec005109d
Ben <128577990+bencoronard@users.noreply.github.com>
2025-02-08 23:52:33 +0700

    Add modal context

--- nextjs-dashboard/src/modules/common/components/closables/form_dialog.tsx
+++ nextjs-dashboard/src/modules/common/components/closables/form_dialog.tsx
@@ -1,64 +1,25 @@
 "use client";
-import React from "react";
 import {
-  Button,
   Dialog,
   DialogActions,
   DialogContent,
-  DialogContentText,
   DialogTitle,
-  TextField,
 } from "@mui/material";
 
 type FormDialogProps = {
   open: boolean;
   title?: string;
-  children: React.ReactNode;
+  content?: React.ReactNode;
+  actions?: React.ReactNode;
+  onClose?: () => void;
 };
 
 export default function FormDialog(props: FormDialogProps) {
-  const [open, setOpen] = React.useState(false);
-
-  const handleClickOpen = () => {
-    setOpen(true);
-  };
-
-  const handleClose = () => {
-    setOpen(false);
-  };
-
   return (
-    <>
-      <Button variant="contained" onClick={handleClickOpen}>
-        Open form dialog
-      </Button>
-
-      <Dialog open={props.open} onClose={handleClose}>
-        <DialogTitle>{props.title}</DialogTitle>
-
-        <DialogContent>
-          <DialogContentText>
-            To subscribe to this website, please enter your email address here.
-            We will send updates occasionally.
-          </DialogContentText>
-          <TextField
-            autoFocus
-            required
-            margin="dense"
-            id="name"
-            name="email"
-            label="Email Address"
-            type="email"
-            fullWidth
-            variant="standard"
-          />
-        </DialogContent>
-
-        <DialogActions>
-          <Button onClick={handleClose}>Cancel</Button>
-          <Button type="submit">Subscribe</Button>
-        </DialogActions>
-      </Dialog>
-    </>
+    <Dialog open={props.open} onClose={props.onClose}>
+      <DialogTitle>{props.title}</DialogTitle>
+      <DialogContent>{props.content}</DialogContent>
+      <DialogActions>{props.actions}</DialogActions>
+    </Dialog>
   );
 }
--- nextjs-dashboard/src/modules/common/stores/modals.ts
+++ nextjs-dashboard/src/modules/common/stores/modals.ts
@@ -1,8 +1,30 @@
+import React from "react";
+import { makeAutoObservable } from "mobx";
+
 class ModalContext {
-  bigModalOpen: boolean = false;
-  smallModalOpen: boolean = false;
-  bigModal: { title: string | null; content: React.ReactNode | null } = {
-    title: null,
-    content: null,
-  };
+  bigModal: {
+    open: boolean;
+    title?: string;
+    content?: React.ReactNode;
+    actions?: React.ReactNode;
+  } = { open: false };
+  smallModal: {
+    open: boolean;
+    title?: string;
+    content?: React.ReactNode;
+    actions?: React.ReactNode;
+  } = { open: false };
+
+  setOpenBigModal(open: boolean) {
+    this.bigModal.open = open;
+  }
+  setOpenSmallModal(open: boolean) {
+    this.smallModal.open = open;
+  }
+
+  constructor() {
+    makeAutoObservable(this);
+  }
 }
+
+export const modalContext = React.createContext(new ModalContext());
