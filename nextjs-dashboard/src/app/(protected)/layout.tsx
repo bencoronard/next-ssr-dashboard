eec876dcafea230e498901496bcace08ce1ca8e9
Ben <128577990+bencoronard@users.noreply.github.com>
2025-02-13 12:10:51 +0700

    Remove unused imports

--- nextjs-dashboard/src/app/(protected)/layout.tsx
+++ nextjs-dashboard/src/app/(protected)/layout.tsx
@@ -9,7 +9,6 @@ import {
   Toolbar,
   Typography,
 } from "@mui/material";
-import NavigationList from "@/modules/common/components/sidebar/navigation_list";
 import MenuIcon from "@mui/icons-material/Menu";
 import UserBadge from "@/modules/common/components/header/user_badge";
 import ResponsiveLogo from "@/assets/icons/logo";
@@ -20,7 +19,6 @@ import { sidebarContext } from "@/modules/common/stores/sidebar";
 import FormDialog from "@/modules/common/components/modals/dialog_form";
 import SystemDialog from "@/modules/common/components/modals/dialog_system";
 import NavigationAccordion from "@/modules/common/components/sidebar/navigation_accordion";
-import { Permission, View } from "@/modules/permissions/models/types";
 import { navMenuItems } from "@/modules/common/components/sidebar/types";
 
 type DashboardLayoutProps = Readonly<{
